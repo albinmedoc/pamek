50130694e6d2bd61c016af2bde7610c24c8ac6b2
Albin Médoc <stamline203@gmail.com>
2020-08-26 13:38:07 +0200

    Created sapper project

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import sirv from 'sirv';
+import express from 'express';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+require('dotenv').config();
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+express()
+    .use(express.json())
+    .use(
+        compression({ threshold: 0 }),
+        sirv('static', { dev }),
+        sapper.middleware()
+    )
+    .listen(PORT, (err) => {
+        if (err) console.log('error', err);
+    });
