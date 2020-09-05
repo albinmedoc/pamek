0e4820e8f68abdbb3678bbf4adaa6b504794212a
Albin Médoc <stamline203@gmail.com>
2020-09-05 22:33:04 +0200

    Created Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,28 @@
+<style>
+    footer {
+        background-color: #333333;
+        text-align: center;
+        padding: 20px 10px;
+    }
+
+    .logo {
+        width: 300px;
+        margin-bottom: 20px;
+    }
+
+    p, p > a {
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
+
+<footer>
+    <img
+        class="logo"
+        src="/images/logo.svg"
+        alt="The Coffee Lounge"
+        title="Pamek VVS" />
+    <p>Traversvägen 1 | 245 34 Staffanstorp</p>
+    <p><a href="tel:+46703774603">+46 703 77 46 03</a></p>
+    <p><a href="mailto:info@pamek.nu">info@pamek.nu</a></p>
+</footer>
