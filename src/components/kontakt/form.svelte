afda645aff05b60b794be4402708a4463e8cffd5
Albin Médoc <stamline203@gmail.com>
2020-09-08 22:55:43 +0200

    Created contact page

---
+++ src/components/kontakt/form.svelte
@@ -0,0 +1,52 @@
+<script>
+    import {
+        Form,
+        FormRow,
+        InputContainer,
+        autoresize,
+    } from 'svelte-responsive-form';
+</script>
+
+<style>
+    div :global(form) {
+        width: auto;
+        padding: 25px 35px;
+        background-color: #e3e3e3;
+        max-width: 300px;
+    }
+</style>
+
+<div>
+    <Form>
+        <FormRow>
+            <InputContainer label="Ditt namn">
+                <input type="text" placeholder=" " required />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer label="Din e-post">
+                <input type="email" placeholder=" " required />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer label="Ditt telefonnummer">
+                <input type="tel" placeholder=" " required />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer label="Ämne">
+                <input type="text" placeholder=" " required />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer label="Meddelande">
+                <textarea use:autoresize placeholder=" " required />
+            </InputContainer>
+        </FormRow>
+        <FormRow>
+            <InputContainer>
+                <input type="submit" value="Skicka">
+            </InputContainer>
+        </FormRow>
+    </Form>
+</div>
---
+++ src/routes/kontakt/index.svelte
@@ -0,0 +1,123 @@
+<script>
+    import Form from '../../components/kontakt/form.svelte';
+</script>
+
+<style>
+    h3 {
+        font-size: 30px;
+        color: #333333;
+    }
+
+    p, a {
+        color: #333333;
+    }
+
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 10%;
+    }
+    
+
+    #persons {
+        grid-column: auto / span 2;
+    }
+
+    #persons > div {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        column-gap: 10%;
+    }
+
+    .person {
+        margin-bottom: 30px;
+    }
+
+    .person > div {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    .person > div > img {
+        width: 200px;
+        border-radius: 30px;
+    }
+
+    @media (max-width: 930px) {
+        .container {
+            grid-template-columns: 1fr;
+        }
+
+        #persons {
+            grid-column: unset;
+        }
+
+        #persons > div {
+            grid-template-columns: 1fr;
+        }
+    }
+
+</style>
+
+<div class="container">
+    <div id="info">
+        <h3>Kontakta oss</h3>
+        <p>
+            Ring eller maila oss när som helst under dagen, vi strävar efter att
+            återkoppla inom 24 timmar!
+        </p>
+        <p>
+            Önskar du en gratis offert? Tveka inte att kontakta oss för att boka
+            ett hembesök. Det är kostnadsfritt och utan krav på beställning
+        </p>
+        <br />
+        <strong>Information</strong>
+        <ul>
+            <li><p>PAMEK AB</p></li>
+            <li><p>Traversvägen 1 | 245 34 Staffanstorp</p></li>
+            <li><p><a href="tel:+46703774603">+46 703 77 46 03</a></p></li>
+            <li><p><a href="mailto:info@pamek.nu">info@pamek.nu</a></p></li>
+        </ul>
+    </div>
+
+    <Form />
+
+    <div id="persons">
+        <h3>Våra montörer</h3>
+        <div>
+            <section class="person">
+                <div>
+                    <img alt="Patrik Takkula" src="/images/mikael_bjorklund.png">
+                    <span>
+                        <p><strong>Patrik Takkula</strong></p>
+                        <p><a href="tel:+46703774603">+46 703 77 46 03</a></p>
+                        <p><a href="mailto:patrik@pamek.nu">patrik@pamek.nu</a></p>
+                    </span>
+                </div>
+                <p>Bmlakmkslmkldm lskamkl mdaklmdlk malkdm la
+                    D ksamkd msalmd lkamskldm las
+                    M kdsamkdm alms kamdlkm akldmslam dlksa
+                    Km dmsakldm aklmdkl amdlksm alkmdlkad</p>
+                    
+                    <p>D kmsamk dmksa dkmaskd masklmd lakmdsam</p>
+            </section>
+            <section class="person">
+                <div>
+                    <img alt="Filip Takkula" src="/images/mikael_bjorklund.png">
+                    <span>
+                        <p><strong>Filip Takkula</strong></p>
+                        <p><a href="tel:+46703774603">+46 703 77 46 03</a></p>
+                        <p><a href="mailto:filip@pamek.nu">filip@pamek.nu</a></p>
+                    </span>
+                </div>
+                <p>Bmlakmkslmkldm lskamkl mdaklmdlk malkdm la
+                    D ksamkd msalmd lkamskldm las
+                    M kdsamkdm alms kamdlkm akldmslam dlksa
+                    Km dmsakldm aklmdkl amdlksm alkmdlkad</p>
+                    
+                    <p>D kmsamk dmksa dkmaskd masklmd lakmdsam</p>
+            </section>
+        </div>
+    </div>
+</div>
