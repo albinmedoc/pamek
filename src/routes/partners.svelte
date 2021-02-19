76646860e87c39b54eb92d37572f70b317767b74
albinmedoc <stamline203@gmail.com>
2021-02-19 22:47:18 +0100

    Created page for partners

---
+++ src/routes/partners.svelte
@@ -0,0 +1,40 @@
+<style>
+    #info {
+        text-align: center;
+    }
+    #info > h3 {
+        font-size: 5vw;
+        margin: 20px 0 0 10px;
+    }
+
+    #info > p {
+        font-size: 2vw;
+    }
+
+    .partner {
+        display: block;
+        text-align: center;
+    }
+    .partner > img {
+        margin: 70px 0;
+        width: 80%;
+        max-width: 400px
+    }
+</style>
+
+<div id="info">
+    <h3>Våra samarbetspartners</h3>
+    <p>Vi är stolta över att presentera våra samarbetspartners.</p>
+</div>
+
+<section id="partners">
+    <a class="partner" href="http://adebbyggkonsulter.se">
+        <img src="/images/partners/ADEB.png" alt="ADEB">
+    </a>
+    <a class="partner" href="http://www.lundskakeltjanst.se/">
+        <img src="/images/partners/LK.png" alt="Lunds Kakeltjänst">
+    </a>
+    <a class="partner" href="http://www.nexabygg.se/">
+        <img src="/images/partners/NEXA.svg" alt="NEXA Bygg">
+    </a>
+</section>
